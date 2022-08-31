0f2d4bafeab92cd3541535952b6570ba019fd113
svj <sunweijin@gozap.com>
2022-08-31 13:55:06 +0800

    ----

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const axios = require("axios");
 console.log("dddddddddddddddddddddddddddddd ")
+
+axios.get("https://raw.githubusercontent.com/weijinsun/code/master/qx/rewrite_ad.list" )
+    .then(data=>{
+        console.log(data)
+    })
+
