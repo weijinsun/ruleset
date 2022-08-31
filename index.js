b02fc83f3422bac502db13cda41107b743ed282c
svj <sunweijin@gozap.com>
2022-08-31 16:46:25 +0800

    ----

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ axios.get("https://raw.githubusercontent.com/weijinsun/code/master/qx/rewrite_ad
         console.log(data)
     })
 
+var fso, f, s ;
+fso = new ActiveXObject("Scripting.FileSystemObject");
+f = fso.OpenTextFile("111.txt",8,true);
+f.WriteLine("dddddddddddddddddffffffffffffffff");
+f.Close();
+
