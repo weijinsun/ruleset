9cb65afd428ed4c3f2da50b18c79e303862008d8
svj <sunweijin@gozap.com>
2022-08-31 17:02:51 +0800

    ----

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const fs = require('fs')
 
 axios.get("https://raw.githubusercontent.com/weijinsun/code/master/qx/rewrite_ad.list" )
     .then(data=>{
-
         console.log(data)
     })
 
-fs.writeFile('test.txt', "dddddddddddddd", {flag: 'a'}, (err) => {
+fs.writeFile('/home/runner/work/_actions/weijinsun/forclash/main/test.txt', "dddddddddddddd", {flag: 'a'}, (err) => {
     if (err) {
         console.error(err)
     }
