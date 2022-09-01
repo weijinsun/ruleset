02b52963ac122be0d172542764bdcccaf4ddfc3a
svj <sunweijin@gozap.com>
2022-09-01 17:34:03 +0800

    ----

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const axios = require("axios");
-const github = require('@actions/github');
-const core = require('@actions/core');
-
 const fs = require('fs')
 
 axios.get("https://raw.githubusercontent.com/weijinsun/code/master/qx/rewrite_ad.list" )
     .then(data=>{
         console.log(data)
     })
+
 
 fs.writeFile('test.txt', "dddddddddddddd", {flag: 'a'}, (err) => {
     if (err) {
