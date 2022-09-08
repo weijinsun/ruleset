419b0ac4285fb6e7761d1ea1227e5af5eeed9a05
svj <sunweijin@gozap.com>
2022-09-08 12:01:41 +0800

    ~~

--- stash_stoverride/test1.js
+++ stash_stoverride/test1.js
@@ -1,8 +1,7 @@
 $httpClient.get("https://api.my-ip.io/ip", function (error, response, data) {
-    const s="aaaaaaaa"
     $done({
         title: "test",
-        content: '<a onclick="alert(${s})">stash toggle</a>',
+        content: '<a onclick="alert(123)">stash toggle</a>',
         backgroundColor: "#663399",
         icon: "network",
     })
